ab925087f17a46dbb7521809e0c95f14374e3556
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2022-10-02 11:39:00 +0300

    Добавьте файлы проекта.

---
+++ 02.10.22 .NET/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._10._22.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] city = { "Arizona", "Michigan ", "New York", "Arizona", "Nebraska" };
+            Console.WriteLine("Масив всіх міст");
+            Console.WriteLine("{0}", string.Join("\n", city));
+
+
+            Console.WriteLine("Міста які починаються на А");
+            var selectedCity = from c in city
+                                 where c.ToUpper().StartsWith("A")
+                                 
+                               orderby c  
+                                 select c;
+
+
+            foreach (string citys in selectedCity)
+                Console.WriteLine(citys);
+            var selectedCitys = from c in city
+                               where c.ToUpper().StartsWith("M")
+
+                               orderby c
+                               select c;
+
+
+            foreach (string citys in selectedCitys)
+                Console.WriteLine(citys);
+            var selectedCiti = from c in city
+                               where c.ToUpper().StartsWith("N")
+
+                               orderby c
+                               select c;
+
+
+            foreach (string citys in selectedCiti)
+                Console.WriteLine(citys);
+
+        }
+    }
+}
